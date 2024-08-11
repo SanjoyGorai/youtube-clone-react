c6e83cafbd1ca91aae41c24b53d7c1ffe68cd889
Mr. Sanjoy <--add>
2024-08-11 19:55:09 +0530

    basics of sidebar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,6 @@ import { BsFillMicFill } from "react-icons/bs";
 import { BiBell, BiSearch, BiSolidCameraPlus } from "react-icons/bi";
 import { SlBell } from "react-icons/sl";
 import { IoMdHome } from "react-icons/io";
-import { SiYoutubeshorts } from "react-icons/si";
-import { MdSubscriptions } from "react-icons/md";
 import { MdHistory } from "react-icons/md";
 import { BiSolidLike } from "react-icons/bi";
 import { BiVideoPlus } from "react-icons/bi";
@@ -20,13 +18,13 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     const logoUrl = `youtube.png`
     return (
-        <div className='flex justify-between   text-black h-20'>
+        <div className='flex justify-between text-black h-20'>
             <div className='flex items-center'>
                 <IoMenuSharp className='cursor-pointer size-8 ps-2 mr-3' />
                 <img src={logoUrl} alt="" className='w-28 h-10 cursor-pointer' />
             </div>
             <div className='flex items-center'>
-                <input type="text" placeholder='Search' className='text-black border border-gray-600 focus:border-black w-96 outline-none h-10 p-2 ps-4 rounded-l-full lg:w-[30rem]' />
+                <input type="text" placeholder='Search' className='text-black border border-gray-600 focus:border-sky-600 w-96 outline-none h-10 p-2 ps-4 rounded-l-full lg:w-[32rem]' />
                 <button><BiSearch className='w-20 bg-white text-black border-black border-r border-y rounded-r-full p-2 size-10' /></button>
                 <BsFillMicFill className='ms-5  p-2 size-10 rounded-full cursor-pointer' />
             </div>
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { IoMdHome } from "react-icons/io";
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdSubscriptions } from "react-icons/md";
+
+
+const Sidebar = () => {
+
+    const navItems = [
+        { title: 'Home', icon: <IoMdHome className='size-6' /> },
+        { title: 'Shorts', icon: <SiYoutubeshorts className='size-6' /> },
+        { title: 'Subscriptions', icon: <MdSubscriptions className='size-6' /> }
+    ]
+
+    return (
+        <div>
+            <ul className='ms-3'>
+                {
+                    navItems.map((item, index) => (
+                        <div className='' key={index}>
+                            <li className='flex items-center gap-4 mt-3 cursor-pointer hover:bg-gray-300 w-48 rounded-xl p-1 '>{item.icon} {item.title} </li>
+                        </div>
+                    ))
+                }
+
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar
